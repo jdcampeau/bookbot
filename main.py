e19bbaa8205b1130f2074d0b10e1322d8a82ee54
jdcampeau <jdcgithub@duck.com>
2025-08-29 05:58:57 -0500

    finished bookbot!

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else: None
+
 from stats import word_char_analysis
 
+word_char_analysis(sys.argv[1])
 
-word_char_analysis("books/frankenstein.txt")
+
 
 
